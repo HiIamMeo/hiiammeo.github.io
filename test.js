40c395df49be017dd7e0fa836017d08834103805
Meo <ltpd175@gmail.com>
2024-01-20 22:35:41 +1100

    update more js

--- test.js
+++ test.js
@@ -12,11 +12,37 @@ function navBar() {
     });
 }
 
-function scrollTo(){
-    const about = document.querySelector('nav-about');
-    const skill = document.querySelector('nav-skill');
-    const exp = document.querySelector('nav-exp');
-    const edu = document.querySelector('nav-edu');
+function scrollToSection() {
+    const about = document.querySelector('.nav-about');
+    const skill = document.querySelector('.nav-skill');
+    const exp = document.querySelector('.nav-exp');
+    const edu = document.querySelector('.nav-edu');
+
+    const scrollToAbout =  document.getElementsByClassName('default-body')[0].offsetTop;
+    const scrollToSkill =  document.getElementsByClassName('default-body')[1].offsetTop;
+    const scrollToExp =  document.getElementsByClassName('default-body')[2].offsetTop;
+    const scrollToEdu =  document.getElementsByClassName('default-body')[3].offsetTop;
+
+    for (let i = 0; i < 4; i++) {
+        about.addEventListener('click', function () {
+            window.scrollTo(0,0);
+            window.scrollTo({ top: scrollToAbout, behavior: 'smooth'});
+        });
+        skill.addEventListener('click', function () {
+            window.scrollTo(0,0);
+            window.scrollTo({ top: scrollToSkill, behavior: 'smooth'});
+        });
+        exp.addEventListener('click', function () {
+            window.scrollTo(0,0);
+            window.scrollTo({ top: scrollToExp, behavior: 'smooth'});
+        });
+        edu.addEventListener('click', function () {
+            window.scrollTo(0,0);
+            window.scrollTo({ top: scrollToEdu, behavior: 'smooth'});
+        });
+    }
 }
 
+
 navBar();
+scrollToSection();
