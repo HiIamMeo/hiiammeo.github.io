2f1a4135886513c5a69f016a1459578d658abbf6
Meo <ltpd175@gmail.com>
2024-01-20 17:56:36 +1100

    push again

--- test.js
+++ test.js
@@ -4,12 +4,19 @@ function navBar() {
     const navBar = document.querySelector('.nav-bar');
     navSiteButton.addEventListener('click', function () {
         navSiteButton.style.display = 'none';
-        navBar.style.display = 'block';
+        navBar.style.left = "0px";
     });
     barButton.addEventListener('click', function () {
         navSiteButton.style.display = 'block';
-        navBar.style.display = 'none';
+        navBar.style.left = '-200px';
     });
+}
+
+function scrollTo(){
+    const about = document.querySelector('nav-about');
+    const skill = document.querySelector('nav-skill');
+    const exp = document.querySelector('nav-exp');
+    const edu = document.querySelector('nav-edu');
 }
 
 navBar();
